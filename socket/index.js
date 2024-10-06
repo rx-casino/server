13718add8c52fd59c4254ba57b49f776ad9bd04a
Highscoretech <highscoretechng@gmail.com>
2024-10-06 10:22:28 -0600

    set server

--- socket/index.js
+++ socket/index.js
@@ -4,7 +4,7 @@ const { Public_Chat } = require("../controllers/public_chat.controller");
 async function createsocket(httpServer){
     const io = new Server(httpServer, {
         cors: {
-          origin: ["https://stroopwafe.netlify.app","http://localhost:5173","http://localhost:5174" ]
+          origin: ["https://rx-casino.netlify.app","http://localhost:5173","http://localhost:5174" ]
         },
     });
     new Public_Chat(io).connection()
